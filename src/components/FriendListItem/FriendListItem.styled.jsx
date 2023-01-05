fab7a50eb25c460717938ca372d6af404085f540
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-01-05 15:00:17 +0200

    Fixed FriendStatus

--- src/components/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -3,19 +3,21 @@ import styled from '@emotion/styled';
 export const FriendCard = styled.li`
   display: flex;
   align-items: center;
-  padding: 4px;
+  padding: 4px 0;
   border-radius: 4px;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
   margin-bottom: 8px;
 `;
 
-export const FriendStatus = styled.span(isOnline => ({
-  width: '12px',
-  height: '12px',
-  borderRadius: '50%',
-  backgroundColor: isOnline ? '#289407' : '#ce1d1d',
-  marginRight: '8px',
-}));
+export const FriendStatus = styled.span`
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  margin: 0 8px;
+  background-color: ${props => {
+    return props.isOnline ? '#289407' : '#ce1d1d';
+  }};
+`;
 
 export const FriendAvatar = styled.img`
   border: 1px solid #b8abab;
